a966a408e94f4f8f35c5aa17c933b37bc686e0ec
Josh Maddigan <josh.maddigan@gmail.com>
2026-05-16 11:31:10 -0400

    Flask server init and first jsonify

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+books = [
+    {"id": 1, "title": "A Court of Thones and Roses", "author": "Sarah J Maas", "genre": "Romantacy", "series": "A Court of Thorns and Roses", "status": "Read", "rating": "5/5", "comments": "dont trust the plot line."},
+    {"id": 2, "title": "Losing Sam", "author": "Nicole Maser", "genre": "Romance", "series": "Losing Sam", "status": "Read", "rating": "5/5", "comments": "best book ever"},
+]
 
+@app.route('/books')
+def get_books():
+    return jsonify(books)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
